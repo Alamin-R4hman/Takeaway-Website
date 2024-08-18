fa83f0cf25cd3fe7e9854df01f0ed23b0d554c93
Alamin Rahman <138202633+Alamin-R4hman@users.noreply.github.com>
2024-08-18 23:57:51 +0100

    Reverted Website
    Reverted back

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,16 +153,6 @@ window.addEventListener("scroll", function (){
 
   });
 
-  /////////////////////////////////////////////////////
-  window.addEventListener("scroll", function () {
-    if (this.scrollY > 50) {
-      header.classList.add("active");
-      backTopBtn.classList.add("active");
-      hideHeader();
-    } else {
-      header.classList.remove("active");
-      backTopBtn.classList.remove("active");
-    }
-  });
+  
 
 
